378b62873825b1148de5a87187541a6a5696b9ed
Andrey <volk000@gmail.com>
2023-02-26 18:32:59 +0600

    Add view for certain article

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -5,6 +5,7 @@ from hexlet_django_blog.article import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name="articles_list"),
+    path('<int:id>/', views.ArticleView.as_view(), name="article"),
     # path('', views.Index.as_view(), name='article'),
     # path(
     #     '<str:tags>/<int:article_id>/',
